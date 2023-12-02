de7f02e7f9744bbf9e73a1a36c61b2b2856d7a94
Ercan Kurt <ercan257@outlook.de>
2023-12-02 20:58:57 +0100

    Weitere Konfigurationen

--- recipleasefrontend/jest.config.js
+++ recipleasefrontend/jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest',
     transform: {
-        '^.+\\.vue$': 'vue-jest'
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.js$': 'babel-jest'
     },
     automock: false,
     resetMocks: false,
     setupFiles: [
         './jest.setup.js'
-    ]
+    ],
+    testMatch: ['<rootDir>/src/components/**/*spec.js']
 }
